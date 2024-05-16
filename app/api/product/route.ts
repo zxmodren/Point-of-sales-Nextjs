14f1a8d3c2bfeb8596e78ca63314763b09a90d43
arya <aryaferdyansahxiii@gmail.com>
2024-05-16 22:06:07 +0700

    add edge

--- app/api/product/route.ts
+++ app/api/product/route.ts
@@ -1,6 +1,7 @@
 import { CatProduct, PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { v4 as uuidv4 } from 'uuid';
+export const runtime = 'edge'
 const prisma = new PrismaClient();
 
 export const POST = async (request: Request) => {
